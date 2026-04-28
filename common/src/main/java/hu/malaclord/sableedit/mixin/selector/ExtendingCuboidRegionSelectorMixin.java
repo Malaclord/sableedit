fb2b0353da323c8b0de9937a7f8ae1fc35e0e0ac
Malaclord <56694430+Malaclord@users.noreply.github.com>
2026-04-28 09:41:18 +0200

    add missing ExtendingCuboidRegionSelectorMixin

---
+++ common/src/main/java/hu/malaclord/sableedit/mixin/selector/ExtendingCuboidRegionSelectorMixin.java
@@ -0,0 +1,43 @@
+package hu.malaclord.sableedit.mixin.selector;
+
+import com.sk89q.worldedit.LocalSession;
+import com.sk89q.worldedit.extension.platform.Actor;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.regions.selector.ExtendingCuboidRegionSelector;
+import com.sk89q.worldedit.regions.selector.limit.SelectorLimits;
+import hu.malaclord.sableedit.RegionSelectorCommon;
+import hu.malaclord.sableedit.RegionSelectorExtended;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ExtendingCuboidRegionSelector.class)
+public abstract class ExtendingCuboidRegionSelectorMixin implements RegionSelectorExtended {
+    @Inject(method = "selectPrimary", at = @At("HEAD"))
+    void selectPrimaryInjected(BlockVector3 position, SelectorLimits limits, CallbackInfoReturnable<Boolean> cir) {
+        RegionSelectorCommon.selectPrimary(this, position, limits, cir);
+    }
+
+    @Inject(method = "selectSecondary", at = @At("HEAD"))
+    void selectSecondaryInjected(BlockVector3 position, SelectorLimits limits, CallbackInfoReturnable<Boolean> cir) {
+        RegionSelectorCommon.selectSecondary(this, position, limits, cir);
+    }
+
+    @Inject(method = "explainPrimarySelection", at = @At("HEAD"))
+    void explainPrimarySelectionInjected(Actor player, LocalSession session, BlockVector3 pos, CallbackInfo ci) {
+        RegionSelectorCommon.explainPrimarySelection(this, player, session, pos, ci);
+    }
+
+    @Redirect(method = "explainPrimarySelection", at = @At(value = "INVOKE", target = "Lcom/sk89q/worldedit/math/BlockVector3;toString()Ljava/lang/String;"))
+    String explainPrimarySelectionBlockVector3ToString(BlockVector3 instance) {
+        return RegionSelectorCommon.blockVector3ToString(this, instance);
+    }
+
+    @Redirect(method = "explainSecondarySelection", at = @At(value = "INVOKE", target = "Lcom/sk89q/worldedit/math/BlockVector3;toString()Ljava/lang/String;"))
+    String explainSecondarySelectionBlockVector3ToString(BlockVector3 instance) {
+        return RegionSelectorCommon.blockVector3ToString(this, instance);
+    }
+}
