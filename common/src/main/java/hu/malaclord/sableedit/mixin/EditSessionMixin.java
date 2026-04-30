aa61e576844ed7c776af40cc7bb2c29c661c087e
Malaclord <56694430+Malaclord@users.noreply.github.com>
2026-04-30 09:48:09 +0200

    slight refractor

--- common/src/main/java/hu/malaclord/sableedit/mixin/EditSessionMixin.java
+++ common/src/main/java/hu/malaclord/sableedit/mixin/EditSessionMixin.java
@@ -8,10 +8,8 @@ import dev.ryanhcode.sable.companion.SubLevelAccess;
 import dev.ryanhcode.sable.sublevel.SubLevel;
 import hu.malaclord.sableedit.Adaptor;
 import hu.malaclord.sableedit.PlayerProxyExtended;
-import hu.malaclord.sableedit.SableEdit;
 import hu.malaclord.sableedit.context.LocationContext;
 import hu.malaclord.sableedit.context.SubLevelContext;
-import net.minecraft.world.level.ChunkPos;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -28,6 +26,7 @@ public class EditSessionMixin {
     @Nullable
     private Actor actor;
 
+    // TODO: Do this better plsplspls
     @Inject(method = "setBlock(Lcom/sk89q/worldedit/math/BlockVector3;Lcom/sk89q/worldedit/world/block/BlockStateHolder;Lcom/sk89q/worldedit/EditSession$Stage;)Z", at = @At("HEAD"))
     <B extends BlockStateHolder<B>> void setBlockInjectedDumbStupid(BlockVector3 position, B block, EditSession.Stage stage, CallbackInfoReturnable<Boolean> cir) {
         if (actor != null && actor.isPlayer() && actor instanceof PlayerProxyExtended playerProxy) {
